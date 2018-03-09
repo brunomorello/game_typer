ae182279543a12dc650ab922627e695c3fa42a5b
Bruno Moreno <brunomorello7@gmail.com>
2018-03-08 22:59:17 -0300

    Added spinner to ajax requests

--- public/assets/js/phrase.js
+++ public/assets/js/phrase.js
@@ -1,6 +1,9 @@
 $("#new-random-phrase").click(newRandomPhrase);
 
 function newRandomPhrase() {
+
+	//active spinner
+	$("#spinner-loading").show();
 	
 	//request array of phrases on localhost
 	$.get("http://localhost:3000/frases", generateRandomPhrase)
@@ -12,6 +15,10 @@ function newRandomPhrase() {
 			//hide error msg after 2 seconds
 			$("#error").toggle();
 		}, 2000);
+	})
+	.always(function () {
+		//when request ends
+		$("#spinner-loading").hide();
 	});
 
 }
